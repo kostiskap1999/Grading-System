35b3ceaaf204ff90dedc2fc85225c3e2e8c18dcd
kostiskap1999 <kostiskap1999@hotmail.com>
2023-12-15 23:11:48 +0200

    Backend: Simplified repository

--- backend/persistence/repository/user.ts
+++ backend/persistence/repository/user.ts
@@ -6,17 +6,19 @@ export class UserRepository {
   constructor(public transactionManager = TransactionManager.createTransaction()) {}
 
   async findById(id: number) {
-    return new User(await this.transactionManager.then(tm => tm.query("SELECT * FROM users WHERE id = ?", id)))
+    const tm = await this.transactionManager
+    return new User(await tm.query("SELECT * FROM users WHERE id = ?", id))
   }
 
   async findUserByCredentials(username: string, password: string) {
-    const credentials = (await this.transactionManager.then(tm => tm.query("SELECT * FROM credentials WHERE username = ? AND password = ?", username, password) as Promise<any[]>))
+    const tm = await this.transactionManager
+    const credentials = (await tm.query("SELECT * FROM credentials WHERE username = ? AND password = ?", username, password) as any[])
       .map(creds => new Credentials(creds))[0]
 
     if (!credentials)
       return null
     
-    return (await this.transactionManager.then(tm => tm.query("SELECT * FROM users WHERE credentials_id = ?", credentials.id) as Promise<any[]>))
+    return (await tm.query("SELECT * FROM users WHERE credentials_id = ?", credentials.id) as any[])
       .map(user => new User(user))[0]
   }
 }
