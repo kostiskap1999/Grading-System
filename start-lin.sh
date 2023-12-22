edfc1c1e6ed5e589eca0138d58b222d5cd531332
kostiskap1999 <kostiskap1999@hotmail.com>
2023-12-22 15:17:19 +0200

    Removed faulty sh files

--- start-lin.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd backend/
-npm run start-lin &
-
-cd ../frontend/
-npm run start-lin
--- start-win.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd backend/
-npm run start-win &
-
-cd ../frontend/
-npm run start-win
