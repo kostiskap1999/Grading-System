201da8e6e9038f2d3d3cca8dece0a361f295858f
kostiskap1999 <kostiskap1999@hotmail.com>
2024-01-15 16:23:35 +0200

    Frontend: Some profile adjustments per role

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -12,7 +12,11 @@ export default function ProfilePage() {
 
   const navigate = useNavigate();
   const [user, setUser] = useState<UserModel>()
-  
+  const [profile, setProfile] = useState<UserModel>()
+
+  const url = new URL(window.location.href)
+  const params = url.searchParams
+
   useEffect(() => {
     const fetchData = async () => {
       const tokenId: number | null = await fetchTokenId()
@@ -25,11 +29,27 @@ export default function ProfilePage() {
     fetchData()
   }, [])
 
+  useEffect(() => {
+    const fetchProfile = async () => {
+      if(user){
+        const parsedId: string = (params.get('id') === null) ? "" : params.get('id')!.toString()
+        if(parseInt(parsedId) === user.id){
+          setProfile(user)
+        }
+        else{
+          const prof: UserModel | null = await fetchAndSetupUser(parseInt(parsedId), 2)
+          prof && setProfile(prof)
+        }
+      }
+    }
+    fetchProfile()
+  }, [user])
+
   return (
     <div className="page column">
       <div className="top-header medium-text center column" style={{flex: 1}}>
         <div></div>
-        <div className="row">{user && `Welcome to your profile ${user.firstName} ${user.lastName}`}</div>
+        <div className="row">{profile && `Welcome to your profile ${profile.firstName} ${profile.lastName}`}</div>
       </div>
       <div className="row" style={{flex: 1}}>
         <div style={{flex: (user && user?.role>1) ? 1 : 3 }}>
@@ -39,55 +59,57 @@ export default function ProfilePage() {
                     <div className="large-text">User Profile</div>
                     <div className='profile-data'>
                         <div>Username</div>
-                        <div>{user?.username}</div>
+                        <div>{profile?.username}</div>
                     </div>
                     <div className='profile-data'>
                         <div>Full Name</div>
-                        <div>{user?.firstName} {user?.lastName}</div>
+                        <div>{profile?.firstName} {profile?.lastName}</div>
                     </div>
                     <div className='profile-data'>
                         <div>Role</div>
-                        <div>{user?.role == 3 ? "Guest" : user?.role == 2 ? "Student" : user?.role == 1 ? "Professor" : user?.role == 0 && "Admin"}</div>
+                        <div>{profile?.role == 3 ? "Guest" : profile?.role == 2 ? "Student" : profile?.role == 1 ? "Professor" : profile?.role == 0 && "Admin"}</div>
                     </div>
                 </div>
                 <div style={{margin: "20px 50px"}}>
                     <div className="large-text">Stats</div>
                     <div className='profile-data'>
                         <div>Joined Subjects</div>
-                        <div>{user?.subjects.length}</div>
+                        <div>{profile?.subjects.length}</div>
                     </div>
                     <div className='profile-data'>
                         <div>Total Projects</div>
-                        <div>{user?.getProjects().length}</div>
+                        <div>{profile?.getProjects().length}</div>
                     </div>
                     <div className='profile-data'>
                         <div>My Submissions</div>
-                        <div>{user?.getSubmissions().length}</div>
+                        <div>{profile?.getSubmissions().length}</div>
                     </div>
                     <div className='profile-data'>
                         <div>Average Grade</div>
-                        <div>{user?.averageGrade ? user?.averageGrade?.toFixed(2) : "No data"}</div>
+                        <div>{profile?.averageGrade ? profile?.averageGrade?.toFixed(2) : "No data"}</div>
                     </div>
                 </div>
               </div>
-              <div className="column">
-                <div style={{margin: "20px 50px"}}>
-                    <div className="large-text">Professor Stats</div>
-                    <div className='profile-data'>
-                        <div>Supervising Subjects</div>
-                        <div>{user?.getSubjects(1).length}</div>
-                    </div>
+              {profile && profile.role <= 1 &&
+                <div className="column">
+                  <div style={{margin: "20px 50px"}}>
+                      <div className="large-text">Professor Stats</div>
+                      <div className='profile-data'>
+                          <div>Supervising Subjects</div>
+                          <div>{profile?.getSubjects(1).length}</div>
+                      </div>
+                  </div>
                 </div>
-              </div>
+              }
             </div>
         </div>
         <div className="row" style={{flex: 1.5}}>
             <div className="column container" style={{flex: 1}}>
             <div className="medium-text center header-title">My Subjects</div>
             <div className="column" style={{overflow:'scroll'}}>
-                {user && user.subjects.map((subject: SubjectModel, index: number) => (
+                {profile && profile.subjects.map((subject: SubjectModel, index: number) => (
                 <button key={index} className="list-button" onClick={() => navigate('/subjects?id=' + subject.id + '&nav-filter=my')}>
-                    <PageButtonDescription component={subject} showGrade={user.role > 1} />
+                    <PageButtonDescription component={subject} showGrade={profile.role > 1} />
                 </button>
                 ))}
             </div>
@@ -96,7 +118,7 @@ export default function ProfilePage() {
             <div className="column container" style={{flex: 1}}>
                 <div className="medium-text center header-title">My Unsubmitted Projects</div>
                 <div className="column" style={{overflow:'scroll'}}>
-                    {user && user.getProjects({filterDeadline: 1}).map((project: ProjectModel, index: number) => (
+                    {profile && profile.getProjects({filterDeadline: 1}).map((project: ProjectModel, index: number) => (
                         <button key={index} className="list-button" onClick={() => navigate('/projects?id=' + project.id)}>
                         <PageButtonDescription component={project} />
                         </button>
